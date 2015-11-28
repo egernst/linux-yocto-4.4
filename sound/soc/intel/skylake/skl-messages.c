7655310af030ffd43157b40cd99c8a79aefee53c
Jeeja KP <jeeja.kp@intel.com>
2015-11-28 15:01:47 +0530

    ASoC: Intel: Skylake: Add helper routines to handle module params
    Some DSP modules have user configurable parameters. These
    parameters are required by modules in the following scenario
    	-  during initialization
    	-  after initialization using set parameter
    
    This patch adds helper routine to set module parameters using
    large config set IPC message and removes params to be passed as
    init module routine.
    
    Signed-off-by: Jeeja KP <jeeja.kp@intel.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/intel/skylake/skl-messages.c
+++ sound/soc/intel/skylake/skl-messages.c
@@ -527,7 +527,7 @@ static void skl_free_queue(struct skl_module_pin *mpin, int q_index)
  * invoke the DSP by sending IPC INIT_INSTANCE using ipc helper
  */
 int skl_init_module(struct skl_sst *ctx,
-			struct skl_module_cfg *mconfig, char *param)
+			struct skl_module_cfg *mconfig)
 {
 	u16 module_config_size = 0;
 	void *param_data = NULL;
@@ -827,4 +827,18 @@ int skl_stop_pipe(struct skl_sst *ctx, struct skl_pipe *pipe)
 	pipe->state = SKL_PIPE_CREATED;
 
 	return 0;
+}
+
+/* Algo parameter set helper function */
+int skl_set_module_params(struct skl_sst *ctx, u32 *params, int size,
+				u32 param_id, struct skl_module_cfg *mcfg)
+{
+	struct skl_ipc_large_config_msg msg;
+
+	msg.module_id = mcfg->id.module_id;
+	msg.instance_id = mcfg->id.instance_id;
+	msg.param_data_size = size;
+	msg.large_param_id = param_id;
+
+	return skl_ipc_set_large_config(&ctx->ipc, &msg, params);
 }
