5b59893024a92510287977c4775fd7dbe617d18d
Jani Nikula <jani.nikula@intel.com>
2016-02-04 18:52:47 +0200

    drm/i915/dsi: skip gpio element execution when not supported
    Skip v3 gpio element because the support is not there, and skip gpio
    element on non-vlv because the sideband code is vlv specific.
    
    v2: the gpio stuff is currently only supported on vlv (Ville)
    
    Cc: drm-intel-fixes@lists.freedesktop.org
    Fixes: 2a33d93486f2 ("drm/i915/bios: add support for MIPI sequence block v3")
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/1454604767-2440-1-git-send-email-jani.nikula@intel.com
    (cherry picked from commit 96afef1d5adee8722549c8c2b788d656ea2ecf21)
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    (cherry picked from commit bfadcded516b121546a2d7bbfea1abe3ce946517)
    Signed-off-by: Wayne Boyer <wayne.boyer@intel.com>

--- drivers/gpu/drm/i915/intel_dsi_panel_vbt.c
+++ drivers/gpu/drm/i915/intel_dsi_panel_vbt.c
@@ -204,6 +204,9 @@ static const u8 *mipi_exec_gpio(struct intel_dsi *intel_dsi, const u8 *data)
 	struct drm_device *dev = intel_dsi->base.base.dev;
 	struct drm_i915_private *dev_priv = dev->dev_private;
 
+	if (dev_priv->vbt.dsi.seq_version >= 3)
+		data++;
+
 	gpio = *data++;
 
 	/* pull up/down */
@@ -211,6 +214,16 @@ static const u8 *mipi_exec_gpio(struct intel_dsi *intel_dsi, const u8 *data)
 
 	if (gpio >= ARRAY_SIZE(gtable)) {
 		DRM_DEBUG_KMS("unknown gpio %u\n", gpio);
+		goto out;
+	}
+
+	if (!IS_VALLEYVIEW(dev_priv)) {
+		DRM_DEBUG_KMS("GPIO element not supported on this platform\n");
+		goto out;
+	}
+
+	if (dev_priv->vbt.dsi.seq_version >= 3) {
+		DRM_DEBUG_KMS("GPIO element v3 not supported\n");
 		goto out;
 	}
 
@@ -229,9 +242,10 @@ static const u8 *mipi_exec_gpio(struct intel_dsi *intel_dsi, const u8 *data)
 
 	/* pull up/down */
 	vlv_gpio_nc_write(dev_priv, pad, val);
-	mutex_unlock(&dev_priv->sb_lock);
 
 out:
+	mutex_unlock(&dev_priv->sb_lock);
+
 	return data;
 }
 
